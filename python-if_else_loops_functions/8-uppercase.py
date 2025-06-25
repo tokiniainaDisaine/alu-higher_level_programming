536cc592b6624cb4d4f0243853f9d1d3f501ea31
tokiniainaDisaine <t.disaine@alustudent.com>
2025-06-26 01:06:13 +0200

    correct 8 and 9

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,11 +3,6 @@ def uppercase(str):
     for char in str:
         is_char = False
         if ord(char) >= 97 and ord(char) <= 122:
-            is_char = True
-        else:
-            continue
-
-        if is_char:
-            print("{}".format(chr(char)), end="")
-        else:
-            print(f"{char}", end="")  
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
+    print("\n")
