a07f5b5936d0332f232aa66fac819a3770fcad76
tokiniainaDisaine <t.disaine@alustudent.com>
2025-06-26 00:28:07 +0200

    correct 2 and 3

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print(f"abcdefghijklmnopqrstuvwxyz")
+
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-print("abcdfghijklmnoprstuvwxyz")
 
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
