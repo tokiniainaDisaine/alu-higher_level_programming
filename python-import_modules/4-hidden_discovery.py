3ac1b396aabf5cc8425e8062f2437f68d5c9a50b
tokiniainaDisaine <t.disaine@alustudent.com>
2025-06-26 11:51:31 +0200

    correct 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-import hidden_4
 
-# idk
+if __name__ == "__main__":
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
