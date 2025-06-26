9ae53233a3aecb7a79b8665fa0863835f631bf52
tokiniainaDisaine <t.disaine@alustudent.com>
2025-06-26 11:33:42 +0200

    correct 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,11 +3,13 @@ if __name__ == "__main__":
     from sys import argv
     n = len(argv)
 
-    if (n-1) == 1 or (n-1) == 0:
-        argument = 'argument'
+    if (n-1) == 1:
+        argument = 'argument:'
+    elif (n-1) == 0:
+        argument = 'argument.'
     else:
-        argument = 'arguments'
+        argument = 'arguments:'
 
-    print(f"{n-1} {argument}:")
+    print(f"{n-1} {argument}")
     for i in range(1, n):
         print(f"{i}: {argv[i]}")
