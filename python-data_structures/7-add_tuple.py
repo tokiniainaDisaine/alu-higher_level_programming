dd91a6232c6566d74bb0dd918f9a5a36a890179c
tokiniainaDisaine <t.disaine@alustudent.com>
2025-06-26 16:33:28 +0200

    correct 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    element = [
-        [tuple_a[0], tuple_a[1]],
-        [tuple_b[0], tuple_b[1]]
-        ]
-    
-    first_element = element_0_0 + element_1_0
-    second_element = element_0_1 + element_1_1
-
+    element = [[]]
+    element[0][0] = tuple_a[0] if len(tuple_a) > 1 else 0
+    element[0][1] = tuple_a[1] if len(tuple_a) > 2 else 0
+    element[1][0] = tuple_b[0] if len(tuple_b) > 1 else 0
+    element[1][1] = tuple_b[1] if len(tuple_b) > 2 else 0
+    first_element = element[0][0] + element[1][0]
+    second_element = element[0][1] +  element[1][1]
     new_tuple = (first_element, second_element)
     return new_tuple
