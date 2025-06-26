261c828ab85f996fefbfa07cd597e4fdcc0aa070
tokiniainaDisaine <t.disaine@alustudent.com>
2025-06-27 00:37:29 +0200

    correct 8

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     if not a_dictionary[key]:
-        pass
+        return a_dictionary
     else:
         del a_dictionary[key]
-    return a_dictionary
+        return a_dictionary
