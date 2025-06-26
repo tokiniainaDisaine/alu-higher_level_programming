9b462fadffcd830878e33e505dc42c5b90d63fad
tokiniainaDisaine <t.disaine@alustudent.com>
2025-06-27 00:07:00 +0200

    correct 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list
+    new_list = my_list.copy()
+
     for i in new_list:
         if i == search:
             i = replace
         else:
-            pass
+            continue
+
     return new_list
