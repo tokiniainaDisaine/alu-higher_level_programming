df72cee0b86fd6bd4c10ffab6b79ebb63cc60679
tokiniainaDisaine <t.disaine@alustudent.com>
2025-06-26 11:25:10 +0200

    correct 2 and 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import sys
-n = len(sys.argv)
+from sys import argv
+n = len(argv)
 result = 0
 for i in range(1, n):
-    number = int(sys.argv[i])
+    number = int(argv[i])
     result = result + number
 print(result)
