5e31c85536c719b86da810adfb183eae7b4d8eca
tokiniainaDisaine <t.disaine@alustudent.com>
2025-06-26 23:54:22 +0200

    correct 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,5 @@ def square_matrix_simple(matrix=[]):
     new_matrix = [[]]
     for index_1, row in enumerate(matrix):
         for index_2, col in enumerate(row):
-            new_matrix[index_1][index_2] = col**2
+            new_matrix[index_1][index_2].append(col**2)
     return new_matrix
