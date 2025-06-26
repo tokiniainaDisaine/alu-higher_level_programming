cd4d4dbb7cdee0a75ec0054e5bd256945a28c55e
tokiniainaDisaine <t.disaine@alustudent.com>
2025-06-27 00:04:21 +0200

    correct 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,6 +4,6 @@ def square_matrix_simple(matrix=[]):
     matrix_len = len(matrix)
 
     for i in range(0, matrix_len):
-        new_matrix[i] = list(map(lambda x: x**2), matrix[i])
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
 
     return (new_matrix)
