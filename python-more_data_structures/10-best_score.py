8bb30a4976e2747298a24757398e98130e5a358c
tokiniainaDisaine <t.disaine@alustudent.com>
2025-06-27 00:50:57 +0200

    correct 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) < 1 or a_dictionary == None:
+    if a_dictionary == None or len(a_dictionary) < 1:
         return None
 
     myKeys = list(a_dictionary.keys())
