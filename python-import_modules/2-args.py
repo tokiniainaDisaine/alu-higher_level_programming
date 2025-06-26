0418737bb898fed7ff7feb15b6bd41465f739d94
tokiniainaDisaine <t.disaine@alustudent.com>
2025-06-26 11:24:40 +0200

    correct 1

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import sys
-n = len(sys.argv)
+from sys import argv
+n = len(argv)
 print(f"{n} arguments:")
 for i in range(1, n):
-    print(f"{i} {sys.argv[i]}")
+    print(f"{i} {argv[i]}")
