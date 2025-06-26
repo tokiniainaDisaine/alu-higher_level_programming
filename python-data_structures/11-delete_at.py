3750aef1507fb5410b0f22fd93558c63b9dce219
tokiniainaDisaine <t.disaine@alustudent.com>
2025-06-26 17:08:55 +0200

    correct 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def delete_at(my_list=[], idx=0):
     new_list = []
     j = 0
@@ -7,6 +6,6 @@ def delete_at(my_list=[], idx=0):
         if i == idx: 
             pass
         else:
-            new_list.append(my_list[i])
-    my_list = new_list
+            new_list.append(my_list[i]) 
+    my_list = new_list.copy
     return new_list
