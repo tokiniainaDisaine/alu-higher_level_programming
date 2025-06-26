e588ca39c5e9c92b1275e0fbf3e93fc506c19dfd
tokiniainaDisaine <t.disaine@alustudent.com>
2025-06-27 00:47:16 +0200

    correct 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,8 +3,10 @@ def best_score(a_dictionary):
     new_dictionary = a_dictionary.copy()
     myKeys = list(a_dictionary.keys())
     max = new_dictionary[myKeys[0]]
+    max_key = myKeys[0]
 
     for i in myKeys:
         if new_dictionary[i] > max:
-            max = new_dictionary[i]
-    return max
+            # max = new_dictionary[i]
+            max_key = i
+    return max_key
