e64143fb499fc560638e8db55db79ae864dac78e
tokiniainaDisaine <t.disaine@alustudent.com>
2025-06-27 00:10:39 +0200

    correct 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
+    length = len(my_list)
 
-    for i in new_list:
-        if i == search:
-            i = replace
+    for i in range(0, length):
+        if my_list[i] == search:
+            new_list[i] = replace
         else:
             continue
 
