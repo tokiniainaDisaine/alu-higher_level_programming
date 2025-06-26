d7ff86dab8859f34ed044b49f94fc7eee8a18ca2
tokiniainaDisaine <t.disaine@alustudent.com>
2025-06-27 00:20:42 +0200

    correct 2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     unique_elements = list(set(my_list))
-    print(unique_elements)
-    return unique_elements
+    total = 0
+
+    for i in unique_elements:
+        total = total + i
+        
+    print("Total: {:d}".format(total))
