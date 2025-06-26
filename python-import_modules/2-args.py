57b3c5c691ed4be170d2616ddb326087f5c09078
tokiniainaDisaine <t.disaine@alustudent.com>
2025-06-26 11:26:11 +0200

    correct 2 and 3

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
-n = len(argv)
-print(f"{n} arguments:")
-for i in range(1, n):
-    print(f"{i} {argv[i]}")
+if __name__ == "__main__":
+    from sys import argv
+    n = len(argv)
+    print(f"{n} arguments:")
+    for i in range(1, n):
+        print(f"{i} {argv[i]}")
--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from sys import argv
-n = len(argv)
-result = 0
-for i in range(1, n):
-    number = int(argv[i])
-    result = result + number
-print(result)
+if __name__ == "__main__":
+    from sys import argv
+    n = len(argv)
+    result = 0
+    for i in range(1, n):
+        number = int(argv[i])
+        result = result + number
+    print(result)
