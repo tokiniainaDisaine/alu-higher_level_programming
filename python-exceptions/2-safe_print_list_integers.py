dea877af66932681fd26d75d4fb2f2441fbe377f
tokiniainaDisaine <t.disaine@alustudent.com>
2025-06-30 19:25:13 +0200

    correct 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    for i in my_list:
+    for i in range(x):
         try:
-            print("{:d}".format(i), end=" " if i != my_list[-1] else '\n')
-        except ValueError:
-            pass
+            print("{:d}".format(i), end="")
+        except IndexError:
+            continue
+    print('')
