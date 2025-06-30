2be6eeb1a0daee3ebd455327f6c68af040cd0570
tokiniainaDisaine <t.disaine@alustudent.com>
2025-06-30 19:08:39 +0200

    correct 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(0,x):
+    for i in range(0,x):
+        try:
             print(my_list[i])
-    except:
-        print("An error occured")
+        except:
+            print("An error occured")
