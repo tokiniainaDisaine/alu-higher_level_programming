6f7d591fe25557aafd990e9ac4e1edf43655e37a
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-01 21:02:54 +0200

    correct 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -55,23 +55,23 @@ class Square:
     def position(self, value):
         """This function allows you to change the value of position"""
 
-        # is_tuple = True if isinstance(value, tuple) else False
+        is_tuple = True if isinstance(value, tuple) else False
 
-        # if isinstance(value[0], int) or value[0] > 0:
-        #     is_int_0 = True
-        # else:
-        #     is_int_0 = False
+        if (isinstance(value[0], int) or value[0] > 0):
+            is_int_0 = True
+        else:
+            is_int_0 = False
 
-        # if isinstance(value[1], int) or value[1] >= 0:
-        #     is_int_1 = True
-        # else:
-        #     is_int_1 = False
+        if (isinstance(value[1], int) or value[1] >= 0):
+            is_int_1 = True
+        else:
+            is_int_1 = False
 
-        # if not is_tuple or len(value) != 2 or not is_int_0 or not is_int_1:
-        if (not isinstance(value, tuple) or
-                len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
+        if (not is_tuple or len(value) != 2 or not is_int_0 or not is_int_1):
+        # if (not isinstance(value, tuple) or
+        #         len(value) != 2 or
+        #         not all(isinstance(num, int) for num in value) or
+        #         not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         
         self._position = value
