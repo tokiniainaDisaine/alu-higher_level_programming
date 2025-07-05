2e7a04204aa00aba5da6d2303c1f8bcca48ffd64
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-05 20:19:11 +0200

    correct 8

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -90,6 +90,6 @@ class Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
 
         if rect_1.area() >= rect_2.area():
-            return rect_1.area()
+            return rect_1
         elif rect_1.area() < rect_2.area():
-            return rect_2.area()
+            return rect_2
