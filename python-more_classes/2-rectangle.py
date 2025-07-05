a65893062f8697dfb00c263b4529d11e03d89c54
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-05 19:29:17 +0200

    correct 2

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -3,7 +3,10 @@
 
 
 class Rectangle:
-    """This is a rectangle class that stores a size and can calculate the area"""
+    """
+    This is a rectangle class that stores
+    a size and can calculate the area
+    """
 
     def __init__(self, width=0, height=0):
         """This function initializes the class"""
@@ -18,13 +21,13 @@ class Rectangle:
         return self.__width
 
     @width.setter
-    def size(self, value):
+    def width(self, value):
         """This function allows you to change the value of width"""
 
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
+            raise TypeError("width must be an integer")
         elif value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("width must be >= 0")
         else:
             self.__width = value
 
@@ -35,13 +38,13 @@ class Rectangle:
         return self.__height
 
     @height.setter
-    def size(self, value):
+    def height(self, value):
         """This function allows you to change the value of height"""
 
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
+            raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("height must be >= 0")
         else:
             self.__height = value
 
