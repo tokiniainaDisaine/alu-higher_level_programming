9e6221d71ab0b7ad292ef71db5305dd52d46242a
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-05 20:23:05 +0200

    correct 9

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -14,8 +14,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """This function initializes the class"""
 
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
