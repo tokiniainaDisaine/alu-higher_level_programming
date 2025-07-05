74fbea42808319401e4c66983da26f7568be1931
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-05 19:46:28 +0200

    correct 3

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -68,5 +68,5 @@ class Rectangle:
         for i in range(self.__height):
             for j in range(self.__width):
                 result += "#"
-            result += "\n"
+            result += "\n" if i != (self.__height-1) else ""
         return result
