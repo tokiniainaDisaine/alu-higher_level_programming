47a6c97b5dfbdfa2be93f5e92efb06cc890c8296
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-07 22:57:42 +0200

    correct 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,9 +12,9 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """This function check if the value is valid"""
         if isinstance(value, int):
-            raise TypeError("<name> must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         elif value < 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
         else:
             return value
 # idk tbh
