f4ca92bf4415f0a708090d6711c82433a97a1c7b
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-07 22:37:57 +0200

    fill text file

---
+++ python-inheritance/1-my_list_copy.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+MyList = __import__('1-my_list').MyList
+
+my_list = MyList()
+my_list.append(1)
+my_list.append(4)
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+
+print(my_list)
+print("\n")
+my_list.print_sorted()
+print("\n")
+print(my_list)
