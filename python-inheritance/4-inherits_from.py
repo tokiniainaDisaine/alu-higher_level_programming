c77c0edad28fe8f0be6c3c278847fd67880cb7aa
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-07 22:49:25 +0200

    correct 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This function that returns True if the object is
+function that returns True if the object is
 an instance of a class that inherited (directly or indirectly) 
 from the specified class ; otherwise False.
 """
@@ -8,7 +8,7 @@ from the specified class ; otherwise False.
 
 def inherits_from(obj, a_class):
     """
-    This function that returns True if the object is
+    function that returns True if the object is
     an instance of a class that inherited (directly or indirectly) 
     from the specified class ; otherwise False.
     """
