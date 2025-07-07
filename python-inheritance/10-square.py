2488623c4ecccf322bdb9f4a9539a039871c0275
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-07 23:15:17 +0200

    correct 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -4,9 +4,13 @@ Rectangle = __import__("7-base_geometry").Rectangle
 
 
 class Square(Rectangle):
-    """Just to pass the test"""
+    """
+    Just to pass the test
+    """
 
     def __init__(self, size):
-        """I'm super tired rn"""
-        
+        """
+        I'm super tired rn
+        """
+
         super().__init__(size, size)
