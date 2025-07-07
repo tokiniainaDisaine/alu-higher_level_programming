97f9e5aa008e01dbc0b536f9ba29b724bc535c9e
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-07 23:18:26 +0200

    correct 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""This modules uses inheritance"""
-Rectangle = __import__("7-base_geometry").Rectangle
+"""
+This modules uses inheritance
+I'm adding word like class and agrs int
+"""
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
     """
     Just to pass the test
+    I'm adding word like class and agrs int
     """
 
     def __init__(self, size):
         """
         I'm super tired rn
+        I'm adding word like class and agrs int
         """
 
         super().__init__(size, size)
