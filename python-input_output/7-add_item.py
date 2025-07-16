c63951ec9b3f989c45f5326fb9844bee57e07c74
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-17 00:10:32 +0200

    correct 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,6 +9,11 @@ import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-for arg in sys.argv:
-    arg = load_from_json_file(arg)
-    save_to_json_file(arg, "add_item.json")
+def add_item():
+    """
+    This function adds all arguments to a Python list,
+    and then save them to a file:
+    """
+    for arg in sys.argv:
+        arg = load_from_json_file(arg)
+        save_to_json_file(arg, "add_item.json")
