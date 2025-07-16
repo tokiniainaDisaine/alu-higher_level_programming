f24925650b210201545cb466602bbcbe9b60a43d
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-16 23:58:31 +0200

    correct 3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """
-This function returns the JSON representation of 
+This function returns the JSON representation of
 an object (string)
 """
-
-
 import json
+
 
 def to_json_string(my_obj):
     """
-    This function returns the JSON representation of 
+    This function returns the JSON representation of
     an object (string)
     """
 
