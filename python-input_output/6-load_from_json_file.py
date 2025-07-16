d4745a1f7f5ebb51b3f861ff0afc9a29f2206a7a
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-17 00:01:50 +0200

    correct 6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """A function that creates an Object from a “JSON file”"""
-
-
 import json
+
 
 def load_from_json_file(filename):
     """A function that creates an Object from a “JSON file”"""
