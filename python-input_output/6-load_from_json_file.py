8989369a8fb91ef4dd83df658d3e587783c15dde
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-17 00:02:45 +0200

    correct 6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,5 +6,5 @@ import json
 def load_from_json_file(filename):
     """A function that creates an Object from a “JSON file”"""
 
-    with open(filename):
-        return json.loads(filename)
+    with open(filename, 'r') as file:
+        return json.loads(file)
