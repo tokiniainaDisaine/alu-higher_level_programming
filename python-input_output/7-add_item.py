0698605e4b8875a3267277d19fa3e56c6c971858
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-17 00:20:38 +0200

    correct 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 This module adds all arguments to a Python list,
- and then save them to a file:
+ and then save them to a file
 """
 import sys
 
@@ -13,13 +13,13 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 def add_item():
     """
     This function adds all arguments to a Python list,
-    and then save them to a file:
+    and then save them to a file
     """
 
     try:
         args = load_from_json_file("add_item.json")
     except Exception:
-        args = []
+        pass
     args += sys.argv[1:]
 
     save_to_json_file(args, "add_item.json")
