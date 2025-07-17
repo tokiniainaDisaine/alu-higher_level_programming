c9972ec2f44eebeed457370d2235e6d1398824ac
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-17 20:02:53 +0200

    correct 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,7 @@
 -- commment
-SELECT tv_genres.name, COUNT(genre_id)
-FROM tv_show_genres
-LEFT JOIN tv_genres
-ON tv_genres.id = tv_show_genres.genre_id;
+SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.show_id) AS 'number_of_shows' 
+FROM tv_genres
+INNER JOIN tv_show_genres 
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
