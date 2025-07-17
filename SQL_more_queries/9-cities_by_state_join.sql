0ebbcbf2c61e8eec1fde421b6b14780ab74c1ad6
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-17 19:54:58 +0200

    correct 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -2,5 +2,5 @@
 SELECT cities.id, cities.name, states.name
 FROM cities
 INNER JOIN states
-ON cites.states_id = states.id
--- ORDER BY cities.id;
+ON cities.states_id = states.id
+ORDER BY cities.id;
