426ada4b51fc5f939e707f5f4b5317600f0f1da4
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-17 20:06:47 +0200

    correct 15

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,8 @@
 -- commment
-SELECT tv_shows.title
-FROM tv_show_genres
+SELECT tv_shows.title FROM tv_show_genres
 LEFT JOIN tv_shows
-ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres
-ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_genres.name = "Comedy";
+    ON tv_shows.id = tv_show_genres.show_id 
+LEFT JOIN tv_genres 
+    ON tv_genres.id = tv_show_genres.genre_id
+    WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;
