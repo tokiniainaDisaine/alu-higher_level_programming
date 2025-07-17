5a72d896f18c91444752a628dcc42e283b57c21e
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-17 20:04:34 +0200

    correct 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,12 @@
 -- commment
-SELECT tv_genres.name
-FROM tv_show_genres
-LEFT JOIN tv_genres
-ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows
-ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shows.name = 'Dexter'
+-- SELECT tv_genres.name FROM tv_show_genres
+-- LEFT JOIN tv_genres
+-- ON tv_genres.id = tv_show_genres.genre_id
+-- LEFT JOIN tv_shows
+-- ON tv_shows.id = tv_show_genres.show_id
+-- WHERE tv_shows.name = 'Dexter'
+SELECT tv_genres.name FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name;
