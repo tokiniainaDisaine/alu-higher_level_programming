6c95c9dfd509e0f82d4cd19b2476955159463622
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-17 02:39:01 +0200

    correct 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,17 +4,10 @@ This module adds all arguments to a Python list,
  and then save them to a file
 """
 import sys
-import importlib
 
 
-# save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-# load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-load_from_json_file = importlib.import_module(
-    "6-load_from_json_file"
-).load_from_json_file
-save_to_json_file = importlib.import_module(
-    "5-save_to_json_file"
-).save_to_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 if __name__ == "__main__":
     """
