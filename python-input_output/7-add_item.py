5b0863079de9aede10fec3085c1e5cb75748bcb1
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-17 02:37:00 +0200

    correct 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -19,10 +19,8 @@ if __name__ == "__main__":
     ).save_to_json_file
 
     try:
-        args = load_from_json_file("add_item.json")
+        items = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        args = []
-
-    # args.extend(sys.argv[1:])
-    args += sys.argv[1:]
-    save_to_json_file(args, "add_item.json")
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
