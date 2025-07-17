895617d106e25bc2ee2ea258d416eabdd4c4bb8f
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-17 20:08:10 +0200

    correct 16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,11 @@
 -- commment
-SELECT tv_shows.title, tv_genres.name
-FROM tv_show_genres
-LEFT JOIN tv_shows
-ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres
-ON tv_genres.id = tv_show_genres.genre_id
+-- SELECT tv_shows.title, tv_genres.name
+-- FROM tv_show_genres
+-- LEFT JOIN tv_shows
+-- ON tv_shows.id = tv_show_genres.show_id
+-- LEFT JOIN tv_genres
+-- ON tv_genres.id = tv_show_genres.genre_id
+SELECT tv_shows.title, tv_genres.name FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name;
