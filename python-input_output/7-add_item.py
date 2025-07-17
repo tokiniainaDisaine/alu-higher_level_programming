713e1a97f8b62da402257aa2890a33facb7701b6
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-17 02:09:40 +0200

    correct 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,7 +10,7 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
-def add_item():
+if __name__ == "__main__":
     """
     This function adds all arguments to a Python list,
     and then save them to a file
@@ -23,6 +23,3 @@ def add_item():
 
     args.extend(sys.argv[1:])
     save_to_json_file(args, "add_item.json")
-
-
-add_item()
