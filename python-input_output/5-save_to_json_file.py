bfb7dcb828283c61a5e1632900f0a5735f19d829
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-17 02:25:27 +0200

    correct 7

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -11,5 +11,5 @@ def save_to_json_file(my_obj, filename):
     This function writes an Object to a text file,
     using a JSON representation
     """
-    with open(filename, 'a') as file:
+    with open(filename, 'a', encoding="utf-8") as file:
         file.write(json.dumps(my_obj))
--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,6 +6,6 @@ import json
 def load_from_json_file(filename):
     """A function that creates an Object from a “JSON file”"""
 
-    with open(filename, 'r') as file:
+    with open(filename, 'r', encoding="utf-8") as file:
         j_file = file.read()
         return json.loads(j_file)
