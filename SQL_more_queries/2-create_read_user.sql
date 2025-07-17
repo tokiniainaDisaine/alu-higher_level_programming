604359b51f5faa01945e9ce64102893709a7b88c
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-17 19:20:33 +0200

    correct 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,4 @@
 -- commment
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2 IDENTIFIED WIHT mysql_native_password BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED WIHT mysql_native_password BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
