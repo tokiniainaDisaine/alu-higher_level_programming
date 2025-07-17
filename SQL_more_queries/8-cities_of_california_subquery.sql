0631c8e4cf40d67b2e48f162ea116ddef511c81a
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-17 19:46:17 +0200

    correct 8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- commment
 SELECT id, name
 FROM ciites
-WHERE state_id IN (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
+WHERE state_id IN (SELECT id FROM states WHERE name = 'California');
