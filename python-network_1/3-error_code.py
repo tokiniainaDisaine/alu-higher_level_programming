4b883fb1ec3dfa4fe9813628af32b96751bf9f8d
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-31 13:25:28 +0200

    correct 3

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -4,9 +4,9 @@ This Python script that takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8).
 """
 import sys
-
 import urllib.request
 import urllib.error
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
@@ -14,4 +14,4 @@ if __name__ == "__main__":
         with urllib.request.urlopen(url) as response:
             print(response.read().decode("utf-8"))
     except urllib.error.HTTPError as e:
-        print("Error code:", e.code)\
+        print("Error code:", e.code)
