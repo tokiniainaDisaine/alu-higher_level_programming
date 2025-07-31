9203c34c3ec0fdf515ffdc23d2d1a0feb10cd1dd
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-31 13:29:42 +0200

    correct 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
-Python script that fetches https://alu-intranet.hbtn.io/status
+This Python script fetches https://alu-intranet.hbtn.io/status
 """
-
 import requests
+
 
 if __name__ == '__main__':
     url = 'https://intranet.hbtn.io/status'
