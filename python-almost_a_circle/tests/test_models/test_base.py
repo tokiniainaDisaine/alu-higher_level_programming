7b97d534d856978f84fdbcbcc7120d928df5a121
tokiniainaDisaine <t.disaine@alustudent.com>
2025-08-01 19:32:18 +0200

    fix 0

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -36,68 +36,68 @@ class TestBase(TestCase):
         self.assertEqual(base_1.id, 2)
         self.assertEqual(base_89.id, 89)
 
-    # def test_to_json_string(self):
-    #     """ 
-    #     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    #     Proin iaculis tellus a felis sodales pulvinar. Mauris tempor
-    #     placerat sem et ultrices. Donec ut dapibus turpis, a feugiat
-    #     Proin finibus pharetra lorem, accumsan sagittis felis pretium
-    #     """
-    #     self.assertEqual(Base.to_json_string(None), "[]")
-    #     self.assertEqual(Base.to_json_string([{'id': 12}]), '[{"id": 12}]')
-    #     self.assertEqual(type(Base.to_json_string([{'id': 12}])), str)
-    #     self.assertEqual(Base.to_json_string([]), "[]")
+    def test_to_json_string(self):
+        """ 
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Proin iaculis tellus a felis sodales pulvinar. Mauris tempor
+        placerat sem et ultrices. Donec ut dapibus turpis, a feugiat
+        Proin finibus pharetra lorem, accumsan sagittis felis pretium
+        """
+        self.assertEqual(Base.to_json_string(None), "[]")
+        self.assertEqual(Base.to_json_string([{'id': 12}]), '[{"id": 12}]')
+        self.assertEqual(type(Base.to_json_string([{'id': 12}])), str)
+        self.assertEqual(Base.to_json_string([]), "[]")
 
-    # def test_save_to_file(self):
-    #     """ 
-    #     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    #     Proin iaculis tellus a felis sodales pulvinar. Mauris tempor
-    #     placerat sem et ultrices. Donec ut dapibus turpis, a feugiat
-    #     Proin finibus pharetra lorem, accumsan sagittis felis pretium
-    #     """
-    #     Base._Base__nb_objects = 0
-    #     Square.save_to_file(None)
+    def test_save_to_file(self):
+        """ 
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Proin iaculis tellus a felis sodales pulvinar. Mauris tempor
+        placerat sem et ultrices. Donec ut dapibus turpis, a feugiat
+        Proin finibus pharetra lorem, accumsan sagittis felis pretium
+        """
+        Base._Base__nb_objects = 0
+        Square.save_to_file(None)
 
-    #     self.assertTrue(os.path.isfile("Square.json"))
-    #     with open("Square.json") as file:
-    #         self.assertEqual(file.read(), '[]')
+        self.assertTrue(os.path.isfile("Square.json"))
+        with open("Square.json") as file:
+            self.assertEqual(file.read(), '[]')
 
-    #     Square.save_to_file([])
-    #     with open("Square.json") as file:
-    #         self.assertEqual(file.read(), '[]')
-    #         self.assertEqual(type(file.read()), str)
+        Square.save_to_file([])
+        with open("Square.json") as file:
+            self.assertEqual(file.read(), '[]')
+            self.assertEqual(type(file.read()), str)
 
-    #     Square.save_to_file([Square(1)])
-    #     with open("Square.json") as file:
-    #         self.assertEqual(file.read(),
-    #                          '[{"id": 1, "size": 1, "x": 0, "y": 0}]')
-    #     Base._Base__nb_objects = 0
+        Square.save_to_file([Square(1)])
+        with open("Square.json") as file:
+            self.assertEqual(file.read(),
+                             '[{"id": 1, "size": 1, "x": 0, "y": 0}]')
+        Base._Base__nb_objects = 0
 
-    #     Rectangle.save_to_file(None)
-    #     self.assertTrue(os.path.isfile("Rectangle.json"))
+        Rectangle.save_to_file(None)
+        self.assertTrue(os.path.isfile("Rectangle.json"))
 
-    #     with open("Rectangle.json") as file:
-    #         self.assertEqual(file.read(), '[]')
+        with open("Rectangle.json") as file:
+            self.assertEqual(file.read(), '[]')
 
-    #     Rectangle.save_to_file([])
-    #     with open("Rectangle.json") as file:
-    #         self.assertEqual(file.read(), '[]')
-    #         self.assertEqual(type(file.read()), str)
+        Rectangle.save_to_file([])
+        with open("Rectangle.json") as file:
+            self.assertEqual(file.read(), '[]')
+            self.assertEqual(type(file.read()), str)
 
-    #     Rectangle.save_to_file([Rectangle(2, 3)])
-    #     with open("Rectangle.json") as file:
-    #         self.assertEqual(file.read(),
-    #                          '[{"id": 1, "width": 2, '
-    #                          '"height": 3, "x": 0, "y": 0}]')
+        Rectangle.save_to_file([Rectangle(2, 3)])
+        with open("Rectangle.json") as file:
+            self.assertEqual(file.read(),
+                             '[{"id": 1, "width": 2, '
+                             '"height": 3, "x": 0, "y": 0}]')
 
-    # def test_from_json_string(self):
-    #     """ 
-    #     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    #     Proin iaculis tellus a felis sodales pulvinar. Mauris tempor
-    #     placerat sem et ultrices. Donec ut dapibus turpis, a feugiat
-    #     Proin finibus pharetra lorem, accumsan sagittis felis pretium
-    #     """
-    #     self.assertEqual(Base.from_json_string(None), [])
-    #     self.assertEqual(Base.from_json_string('[{"id": 89}]'), [{'id': 89}])
-    #     self.assertEqual(type(Base.from_json_string('[{"id": 89}]')), list)
-    #     self.assertEqual(Base.from_json_string("[]"), [])
+    def test_from_json_string(self):
+        """ 
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Proin iaculis tellus a felis sodales pulvinar. Mauris tempor
+        placerat sem et ultrices. Donec ut dapibus turpis, a feugiat
+        Proin finibus pharetra lorem, accumsan sagittis felis pretium
+        """
+        self.assertEqual(Base.from_json_string(None), [])
+        self.assertEqual(Base.from_json_string('[{"id": 89}]'), [{'id': 89}])
+        self.assertEqual(type(Base.from_json_string('[{"id": 89}]')), list)
+        self.assertEqual(Base.from_json_string("[]"), [])
