2f023d3b111d0a314cf957b30fa46214bd9d9ec1
tokiniainaDisaine <t.disaine@alustudent.com>
2025-09-26 20:22:10 +0200

    batch fix

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let myVar_1 = "C is fun";
-let myVar_2 = "Python is cool";
-let myVar_3 = "JavaScript is amazing";
+let myVar_1 = 'C is fun';
+let myVar_2 = 'Python is cool';
+let myVar_3 = 'JavaScript is amazing';
 console.log(myVar_1);
 console.log(myVar_2);
 console.log(myVar_3);
