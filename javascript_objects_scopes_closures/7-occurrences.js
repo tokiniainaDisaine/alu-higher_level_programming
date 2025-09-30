0bcb1474dd09fa3deee6329c151dda924deb11b9
tokiniainaDisaine <t.disaine@alustudent.com>
2025-09-30 23:20:22 +0200

    fix 7

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,7 @@
 exports.nbOccurences = function (list, searchElement) {
   let occurence = 0;
   for (let i = 0; i < list.length; i++) {
-    if (list[i] == searchElement) {
+    if (list[i] === searchElement) {
       occurence++;
     }
   }
