ada37dd6e8712080ca23513047b2e112b2d995d6
tokiniainaDisaine <t.disaine@alustudent.com>
2025-09-30 23:08:36 +0200

    fix 6

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,9 +1,11 @@
-#!/usr/bin/node
-
 module.exports = class Square extends require('./5-square.js') {
-  charPrint (c = 'X') {
-    super.print();
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
   }
-}
-
-module.exports = Square;
+};
