cea9ccd81ed8a94540a55763138f338f185a6249
tokiniainaDisaine <t.disaine@alustudent.com>
2025-09-30 23:51:54 +0200

    fix 8

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let reverse = [];
-  for (let i = list.length -1; i >= 0; i--) {
+  const reverse = [];
+  for (let i = list.length - 1; i >= 0; i--) {
     reverse.push(list[i]);
   }
   return reverse;
