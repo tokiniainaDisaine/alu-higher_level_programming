7f8210090dc183876b5e8d36da2a03e5ba2de2fa
tokiniainaDisaine <t.disaine@alustudent.com>
2025-09-30 22:02:56 +0200

    batch upload

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+class Square extends Rectangle {
+    constructor(size) {
+        this.size = size;
+    }
+
+    charPrint(c="X") {
+        for (let i = 0; i < this.size; i++) {
+            console.log(c.repeat(this.size));
+        }
+    }
+}
---
+++ javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+let count = 0;
+function logMe(item) {
+  console.log(`${count}: ${item}`);
+  count++;
+};
