11c0b50a45abcb6562ab4a951ed1f50c099639af
tokiniainaDisaine <t.disaine@alustudent.com>
2025-09-30 23:03:06 +0200

    fix 5/6

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 class Square extends Rectangle {
   constructor (size) {
-    this.width = size;
-    this.height = size;
+    super(size, size);
   }
 }
 
--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 class Square extends Rectangle {
   constructor (size) {
-    this.size = size;
+    super(size, size);
   }
 
   charPrint (c = 'X') {
-    for (let i = 0; i < this.size; i++) {
-      console.log(c.repeat(this.size));
-    }
+    super.print();
   }
 }
 
