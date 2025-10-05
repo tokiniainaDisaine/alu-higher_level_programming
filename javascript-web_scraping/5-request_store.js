7efd01e4dac30c9fd0132257e34d030c4ddc10dc
tokiniainaDisaine <t.disaine@alustudent.com>
2025-10-05 14:37:44 +0200

    fix 5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
+const fs = require('fs');
 request(process.argv[2], 'utf8', function (error, response, body) {
   if (error) {
     console.error(`Error fetching URL: ${error}`);
