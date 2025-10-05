fe889bc151686e777476d9fc06ed1905eb2f2b2a
tokiniainaDisaine <t.disaine@alustudent.com>
2025-10-05 13:48:07 +0200

    fix 2

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -4,9 +4,6 @@ request('https://jsonplaceholder.typicode.com/todos', function (body) {
   var result = {};
   for (const task of body) {
     result[task[userId]] = 0;
-  }
-
-  for (const task of body) {
     if (task["completed"] === true) {
         result[task[userId]]++; 
     }
