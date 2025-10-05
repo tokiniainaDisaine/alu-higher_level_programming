938ad33e777784059e56ac92a64a9b42ead171aa
tokiniainaDisaine <t.disaine@alustudent.com>
2025-10-05 14:26:21 +0200

    fix 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/';
-request.get(url, (error, response, body) => { 
+request.get(url, (error, response, body) => {
   if (!error) {
     const results = JSON.parse(body).results;
     let count = 0;
     for (const film of results) {
       for (const character of film.characters) {
-        if (character === 'https://swapi-api.alx-tools.com/api/people/18/') {
+        if (character == 'https://swapi-api.alx-tools.com/api/people/18/') {
           count++;
         }
       }
     }
     console.log(count);
-  }   
+  }
 });
