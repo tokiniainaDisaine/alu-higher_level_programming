3bbb6100aa3c7cb6113c073354406475ad39c719
tokiniainaDisaine <t.disaine@alustudent.com>
2025-10-05 14:35:36 +0200

    fix 5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
 request(process.argv[2], 'utf8', function (error, response, body) {
-  console.log(body);
+  if (error) {
+    console.error(`Error fetching URL: ${error}`);
+  } else {
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.error(`Error writing file: ${err}`);
+      }
+    });
+  }
 });
