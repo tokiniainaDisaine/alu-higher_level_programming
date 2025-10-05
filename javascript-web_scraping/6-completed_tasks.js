1dd25e0279cb37febf078065c7e23a794765cda3
tokiniainaDisaine <t.disaine@alustudent.com>
2025-10-05 14:47:03 +0200

    fix 6

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
-  const result = {};
-  for (const task of JSON.parse(body)) {
-    result[task["userId"]] = 0;
-    if (task.completed === true) {
-      result[task["userId"]]++;
+  if (!error) {
+    const result = {};
+    for (const task of JSON.parse(body)) {
+      result[task.userId] = 0;
+      if (task.completed === true) {
+        result[task.userId]++;
+      }
     }
+    console.log(result);
   }
-  console.log(result);
 });
