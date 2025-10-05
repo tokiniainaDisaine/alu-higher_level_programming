4a93daed4b3cccf6fa3eef0971eb4b552e1b789a
tokiniainaDisaine <t.disaine@alustudent.com>
2025-10-05 14:06:20 +0200

    fix 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, function (error, response, body) {
-//   console.error('error:', error);
-  console.log(body["title"]);
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+request.get(url, (error, response) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(response.body)["title"]);
+  }
 });
